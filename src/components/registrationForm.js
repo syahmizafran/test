4f009a4ac1800fd5bf9c1f0ad6af26a8211e7db9
Syahmi Zaini <syahmizaini707@gmail.com>
2023-08-30 03:51:15 +0800

    another edit

--- src/components/registrationForm.js
+++ src/components/registrationForm.js
@@ -33,28 +33,28 @@ function RegistrationForm() {
 
     return(
       <div className="form">
-          <div className="form-body">
-              <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
-              </div>
-              <div className="lastname">
-                  <label className="form__label" for="lastName">Last Name </label>
-                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
-              </div>
-              <div className="email">
-                  <label className="form__label" for="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
-              </div>
-              <div className="password">
-                  <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
-              <div className="confirm-password">
-                  <label className="form__label" for="confirmPassword">Confirm Password </label>
-                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
-              </div>
-          </div>
+            <div className="form-body">
+                <div className="username">
+                    <label className="form__label" for="firstName">First Name </label>
+                    <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
+                </div>
+                <div className="lastname">
+                    <label className="form__label" for="lastName">Last Name </label>
+                    <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
+                </div>
+                <div className="email">
+                    <label className="form__label" for="email">Email </label>
+                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
+                </div>
+                <div className="password">
+                    <label className="form__label" for="password">Password </label>
+                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
+                </div>
+                <div className="confirm-password">
+                    <label className="form__label" for="confirmPassword">Confirm Password </label>
+                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+                </div>
+            </div>
           <div class="footer">
               <button type="button" className="btn btn-primary" onClick={()=>handleSubmit()} >Register</button>
           </div>
