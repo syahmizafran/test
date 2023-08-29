333b60cef40d78950e7e9d768433abf89123e7d1
Syahmi Zaini <syahmizaini707@gmail.com>
2023-08-30 03:05:02 +0800

    edit bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import './components/style.css'
+import './components/style.css';
 import Header from './components/header';
 import RegistrationForm from './components/registrationForm';
 
