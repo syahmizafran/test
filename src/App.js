b61bdf8f433d52ae26a16963f1b0183b78c513bf
Syahmi Zaini <syahmizaini707@gmail.com>
2023-08-30 02:26:45 +0800

    new form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './components/header';
+import RegistrationForm from './components/registrationForm';
 
 function App() {
   return (
     <div className="App"> 
     <Header/>
+    <RegistrationForm/>
 
     </div>
   );
